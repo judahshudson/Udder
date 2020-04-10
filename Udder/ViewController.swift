cbbadb18710093280265f999af4fd6d3ef79c222
Judah Hudson <judahshudson@gmail.com>
2020-04-10 01:22:45 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Judah Hudson on 4/10/20.
+//  Copyright © 2020 Judah Hudson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
